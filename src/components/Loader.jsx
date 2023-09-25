f2e77fda70e92a16d1fe3e263fed40d1bb14b978
Gaban <glt037100@gmail.com>
2023-09-25 18:47:56 +0200

    change title + loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,13 +8,26 @@ const LoadingContainer = styled.div`
   height: 100vh;
 `;
 
-const LoadingSpinner = styled.div`
-  border: 16px solid #f3f3f3;
-  border-top: 16px solid blue;
+const DeathStarSpinner = styled.div`
+  background-color: #333;
+  border: 4px solid #555;
   border-radius: 50%;
   width: 80px;
   height: 80px;
-  animation: spin 2s linear infinite;
+  position: relative;
+  animation: spin 4s linear infinite;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 20%;
+    left: 20%;
+    width: 20px;
+    height: 20px;
+    background-color: #555;
+    border-radius: 50%;
+  }
+
   @keyframes spin {
     0% {
       transform: rotate(0deg);
@@ -25,14 +38,39 @@ const LoadingSpinner = styled.div`
   }
 `;
 
+const LaserBeam = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 4px;
+  height: 20px;
+  background-color: red;
+  animation: beam 1s infinite alternate;
+
+  @keyframes beam {
+    0% {
+      height: 20px;
+    }
+    100% {
+      height: 40px;
+    }
+  }
+`;
+
 const LoadingText = styled.p`
   margin-top: 16px;
+  font-family: "Star Jedi", sans-serif;
+  font-size: 24px;
+  color: #fff;
 `;
 
 const Loading = () => {
   return (
     <LoadingContainer>
-      <LoadingSpinner />
+      <DeathStarSpinner>
+        <LaserBeam />
+      </DeathStarSpinner>
       <LoadingText>Loading...</LoadingText>
     </LoadingContainer>
   );
